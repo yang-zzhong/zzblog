1c06934797439e70f060172833c9894295793dfe
yang-zhong <ironwell.young@gmail.com>
2020-11-08 20:12:22 +0800

    fix bug

--- frontend/src/formatter.js
+++ frontend/src/formatter.js
@@ -4,11 +4,10 @@ export const formatter = {
   format_time: function(time_str) {
     let before = new Date(time_str);
     let now = new Date();
-    let next_day = new Date(new Date().setHours(0, 0, 0, 0)) / 1000 + 3600 * 24;
+    let db = new Date(new Date().setHours(0, 0, 0, 0));
     let sec = 0;
-    if (before.getTime() / 1000 > next_day) {
-      let r = formatter.format_date(time_str);
-      return r;
+    if (before.getTime() < db.getTime()) {
+      return formatter.format_date(time_str);
     }
     sec = (now.getTime() - before) / 1000;
     if (sec <= 3600) {
@@ -25,15 +24,13 @@ export const formatter = {
   },
   format_date: function(time_str) {
     let before = new Date(time_str);
-    let next_day = new Date(new Date().setHours(0, 0, 0, 0)) / 1000 + 3600 * 24;
+    let bs = before.getTime();
+    let tmp = new Date(new Date().setHours(0, 0, 0, 0));
+    let before_day =  tmp / 1000 - 3600 * 24;
     let now = new Date();
-    if (before > next_day) {
-      before = new Date(before.setHours(0, 0, 0, 0));
-    }
-    let sec = (now - before) / 1000;
-    if (sec < 3600 * 24) {
+    if (bs > tmp.getTime()) {
       return strings.today;
-    } if (sec >= 3600 * 24 && sec <= 3600 * 24 * 2) {
+    } else if (bs / 1000 > before_day) {
       return strings.yesterday;
     }
     return strings.formatString(strings.at, {
